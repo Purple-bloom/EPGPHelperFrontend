b3f195e01897d2c954001e120f4b283f0d77f2d2
purple-bloom <github@simon-schindler.de>
2025-11-09 18:40:55 +0100

    Fixing sort on chrome #2

--- src/Services/PlayerService.js
+++ src/Services/PlayerService.js
@@ -7,7 +7,7 @@ export function ListAllPlayers(context) {
         const fetchPlayers = async () => {
             try {
                 const data = await getAllPlayers();
-                data.sort((a, b) => a.prio - b.prio);
+                data.sort((a, b) => b.prio - a.prio);
                 setPlayers(data);
             } catch (error) {
                 console.error('Error fetching players:', error);
