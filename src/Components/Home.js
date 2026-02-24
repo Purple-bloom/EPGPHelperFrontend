63bec3e499daea27be951d6b6d7a06b0481e3f6b
cyd <github@simon-schindler.de>
2026-02-25 00:17:01 +0100

    testing like this is fun, i promise

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
     return (
             <div className="bg-dark text-light app-container">
                 <img src={lootIcon} className="App-logo" alt="logo" />
-                <p className = "textSmall">This is an EPGP helper. This website allows you to award EP and GP by character-to-player matching, add players and their characters, export to a supported addon (like https:\/\/github.com/Purple-bloom/EpgpWhisperer) </p>
+                <p className = "textSmall">This is an EPGP helper. This website allows you to award EP and GP by character-to-player matching, add players and their characters, export to a supported addon (like github.com/Purple-bloom/EpgpWhisperer) </p>
                 <p className = "textSmall">Enjoy!</p>
 
             </div>
