ee15dd7fff65526fb80e3aa3a8831c97d2866839
cyd <github@simon-schindler.de>
2026-03-21 14:51:48 +0100

    mobile menu

--- src/Components/Layout/Navigation.js
+++ src/Components/Layout/Navigation.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Home from "../Home";
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Navigation({ token }) {
     const navigate = useNavigate();
@@ -14,6 +15,18 @@ export default function Navigation({ token }) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
